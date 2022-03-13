cbc66dcfd418a9b2ad25e338970191c860b13429
Enzo Maruffa <enzomm1999@gmail.com>
2022-03-12 21:25:24 -0300

    Initial Commit

---
+++ Elden Ring Tracker/Elden_Ring_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elden_Ring_TrackerApp.swift
+//  Elden Ring Tracker
+//
+//  Created by Enzo Maruffa on 12/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Elden_Ring_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
